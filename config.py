f3f8e149bab94f3a92c0a69b1f47b9403d931b8b
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2012-12-29 12:26:39 -0500

    Use a configuration mapping for scraper config values

---
+++ config.py
@@ -0,0 +1,15 @@
+class Config (dict):
+  def defaults(self, defs):
+    """
+    Update from the keys in defs where this dictionary has no corresponding
+    key.
+    """
+    for key in defs:
+      if key not in self:
+        self[key] = defs[key]
+    return self
+
+
+DEFAULT_CONFIG = Config(
+  date_format='%m/%d/%Y'
+)
--- tests/test_legistar_scraper.py
+++ tests/test_legistar_scraper.py
@@ -3,13 +3,15 @@ from legistar_scraper import LegistarScraper
 
 @istest
 def default_legislation_and_calendar_uris():
-  scraper = LegistarScraper('synecdoche.legistar.com')
+  config = {'hostname': 'synecdoche.legistar.com'}
+  scraper = LegistarScraper(config)
   assert_equal(scraper._legislation_uri, 'http://synecdoche.legistar.com/Legislation.aspx')
   assert_equal(scraper._calendar_uri, 'http://synecdoche.legistar.com/Calendar.aspx')
 
 @istest
 def supports_advanced_initial_search_form():
-  scraper = LegistarScraper('chicago.legistar.com')
+  config = {'hostname': 'chicago.legistar.com'}
+  scraper = LegistarScraper(config)
   summaries = scraper.searchLegislation('')
   try:
     summaries.next()
@@ -18,7 +20,8 @@ def supports_advanced_initial_search_form():
 
 @istest
 def supports_simple_initial_search_form():
-  scraper = LegistarScraper('phila.legistar.com')
+  config = {'hostname': 'phila.legistar.com'}
+  scraper = LegistarScraper(config)
   summaries = scraper.searchLegislation('')
   try:
     summaries.next()
@@ -27,13 +30,15 @@ def supports_simple_initial_search_form():
 
 @istest
 def can_get_legislation_detail_using_summary_row():
-  scraper = LegistarScraper('phila.legistar.com')
+  config = {'hostname': 'phila.legistar.com'}
+  scraper = LegistarScraper(config)
   summaries = scraper.searchLegislation('')
   first_summary = summaries.next()
   first_detail = scraper.expandLegislationSummary(first_summary)
   assert_equal(first_detail[0]['Title'], first_summary['Title'])
 
-  scraper = LegistarScraper('chicago.legistar.com')
+  config = {'hostname': 'chicago.legistar.com'}
+  scraper = LegistarScraper(config)
   summaries = scraper.searchLegislation('')
   first_summary = summaries.next()
   first_detail = scraper.expandLegislationSummary(first_summary)
@@ -41,7 +46,8 @@ def can_get_legislation_detail_using_summary_row():
 
 @istest
 def parse_detail_keys():
-  scraper = LegistarScraper('phila.legistar.com')
+  config = {'hostname': 'phila.legistar.com'}
+  scraper = LegistarScraper(config)
   summary = {'URL':'http://phila.legistar.com/LegislationDetail.aspx?ID=1265815&GUID=97CBBF7C-A123-4808-9D50-A1E340BE5BC1'}
   detail = scraper.expandLegislationSummary(summary)
   assert_in(u'Version', detail[0].keys())
@@ -49,8 +55,18 @@ def parse_detail_keys():
 
 @istest
 def recognize_dates():
-  scraper = LegistarScraper('phila.legistar.com')
+  config = {'hostname': 'phila.legistar.com', 'date_format': '%m/%d/%Y'}
+  scraper = LegistarScraper(config)
   summaries = scraper.searchLegislation('')
   summary = summaries.next()
   import datetime
   assert_is_instance(summary['File Created'], datetime.datetime)
+
+@istest
+def ignore_dates_when_no_date_format():
+  config = {'hostname': 'phila.legistar.com'}
+  scraper = LegistarScraper(config)
+  summaries = scraper.searchLegislation('')
+  summary = summaries.next()
+  import datetime
+  assert_not_is_instance(summary['File Created'], datetime.datetime)
