3abbf4e9652bf58b067a26d0bb1f5941d8b8a617
Forest Gregg <fgregg@uchicago.edu>
2013-01-14 09:30:10 -0600

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from distutils.core import setup, Extension
+
+setup(
+    name='legistar-scraper',
+    version='0.1',
+    packages=['legistar'],
+    )
