118a95cfdfa98258cfb2a2c6a68f845b7ef1d965
Paul Tagliamonte <tag@pault.ag>
2013-05-01 12:03:29 -0400

    Pupa core grew legislator objects

--- boston/__init__.py
+++ boston/__init__.py
@@ -6,7 +6,7 @@
 #    - Paul Tagliamonte <paultag@sunlightfoundation.com>
 
 
-from pupa.scrape import Jurisdiction, Scraper
+from pupa.scrape import Jurisdiction, Scraper, Legislator
 from larvae.person import Person
 from larvae.organization import Organization
 
@@ -39,28 +39,6 @@ class Boston(Jurisdiction):
 
     def scrape_session_list(self):
         return ['2013']
-
-
-class Legislator(Person):
-    _type = 'person'
-    _is_legislator = True
-    __slots__ = ('district', 'party', 'chamber', '_contact_details')
-
-    def __init__(self, name, district, party=None, chamber=None, **kwargs):
-        super(Legislator, self).__init__(name, **kwargs)
-        self.district = district
-        self.party = party
-        self.chamber = chamber
-        self._contact_details = []
-
-    def add_contact(self, type, value, group):
-        self._contact_details.append({'type': type, 'value': value,
-                                      'group': group})
-
-    def add_committee_membership(self, com_name, role='member'):
-        org = Organization(com_name, classification='committee')
-        self.add_membership(org, role=role)
-        self._related.append(org)
 
 
 class BostonPersonScraper(Scraper):
