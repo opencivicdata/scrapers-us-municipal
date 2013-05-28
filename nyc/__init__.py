0d12fe771ad8ee507fb6a2551017f227000d17f9
Paul Tagliamonte <tag@pault.ag>
2013-05-28 11:23:06 -0400

    reflow nyc scrapers

--- nyc/__init__.py
+++ nyc/__init__.py
@@ -6,23 +6,10 @@
 #    - Paul Tagliamonte <paultag@sunlightfoundation.com>
 
 
-from pupa.scrape import Jurisdiction, Scraper, Legislator
-from larvae.organization import Organization
-from larvae.person import Person
-from larvae.event import Event
+from pupa.scrape import Jurisdiction
 
-import datetime as dt
-import lxml.html
-import re
-
-
-
-
-MEMBER_PAGE = "http://council.nyc.gov/html/members/members.shtml"
-COMMITTEE_BASE = "http://council.nyc.gov/includes/scripts"
-COMMITTEE_PAGE = "{COMMITTEE_BASE}/nav_nodes.js".format(**locals())
-JS_PATTERN = re.compile(r"\s+\['(?P<name>.*)','(?P<url>.*)',\],")
-
+from .events import NewYorkCityEventsScraper
+from .people import NewYorkCityPersonScraper
 
 
 class NewYorkCity(Jurisdiction):
@@ -35,7 +22,7 @@ class NewYorkCity(Jurisdiction):
                 'terms': [{'name': '2013-2014', 'sessions': ['2013'],
                            'start_year': 2013, 'end_year': 2014
                           }],
-                'provides': ['person'],
+                'provides': ['people', 'events'],
                 'parties': [{'name': 'Democrat' },
                             {'name': 'Republican'},
                             {'name': 'other'}],
@@ -44,7 +31,7 @@ class NewYorkCity(Jurisdiction):
 
     def get_scraper(self, term, session, scraper_type):
         scrapers = {
-            "person": NewYorkCityPersonScraper,
+            "people": NewYorkCityPersonScraper,
             "events": NewYorkCityEventsScraper
         }
         if scraper_type in scrapers:
@@ -52,165 +39,3 @@ class NewYorkCity(Jurisdiction):
 
     def scrape_session_list(self):
         return ['2013']
-
-
-class NewYorkCityPersonScraper(Scraper):
-    def lxmlize(self, url, encoding='utf-8'):
-        entry = self.urlopen(url).encode(encoding)
-        page = lxml.html.fromstring(entry)
-        page.make_links_absolute(url)
-        return page
-
-    def get_people(self):
-        yield self.nyc_scrape_committees()
-        yield self.nyc_scrape_people()
-
-    def scrape_homepage(self, homepage):
-        ret = {}
-
-        page = self.lxmlize(homepage)
-
-        ret['image'] = page.xpath(
-            "//td[@class='inside_top_image']//img")[0].attrib['src']
-
-        bio = page.xpath("//td[@class='inside_sub_feature']")[0]
-        ret['bio'] = bio.text_content()
-
-        #infobox = page.xpath("//td[@class='inside_top_text']")[0]
-        #infobits = infobox.xpath(".//strong")
-        #for entry in infobox:
-        #    k, v = entry.text, entry.tail
-        #    if k == "Committees":
-        #        print v
-
-        return ret
-
-
-    def get_committees(self):
-        page = self.urlopen(COMMITTEE_PAGE)
-        active = False
-        for line in page.splitlines():
-            if "['Committees','" in line:
-                active = True
-                continue
-            if active and not line.endswith("',],"):
-                active = False
-                continue
-
-            if active:
-                line = JS_PATTERN.match(line)
-                if line is None:
-                    continue
-
-                ret = line.groupdict()
-                encoding_lame_bits = {
-                    "\\'": "'"
-                }
-                for k, v in encoding_lame_bits.items():
-                    ret['name'] = ret['name'].replace(k, v)
-
-                ret['url'] = "%s/%s" % (COMMITTEE_BASE, ret['url'])
-                yield ret
-
-
-    def scrape_committee_homepage(self, url):
-        ret = {"people": []}
-        page = self.lxmlize(url, encoding='utf-8')
-        people = page.xpath("//td[@class='inside_sub_feature']//p")
-        if len(people) != 3:
-            print "EMPTY COMMITTEE: ", url
-            return ret
-
-        people = people[1]
-        ret['people'] = people.text_content().split("\n")
-        return ret
-
-
-    def nyc_scrape_committees(self):
-        for committee in self.get_committees():
-            name, url = [committee[x] for x in ["name", "url"]]
-            info = self.scrape_committee_homepage(url)
-            c = Organization(name=name,
-                             classification='committee')
-            c.add_source(url)
-            # c.add_member(x) for x in info['people']
-            yield c
-
-
-    def nyc_scrape_people(self):
-        page = self.lxmlize(MEMBER_PAGE)
-        for entry in page.xpath("//table[@id='members_table']//tr"):
-            entries = entry.xpath(".//td")
-            if entries == []:
-                continue
-
-            name, district, borough, party = entries
-            name = name.xpath(".//a")[0]
-            homepage = name.attrib['href']
-            name, district, borough, party = [x.text for x in
-                                              [name, district, borough, party]]
-
-            info = self.scrape_homepage(homepage)
-            p = Legislator(name=name,
-                           district=district,
-                           # borough=borough,
-                           party=party.strip() or "other")
-            p.add_link(homepage, 'homepage')
-            p.add_source(homepage)
-            p.add_source(MEMBER_PAGE)
-            yield p
-
-
-class NewYorkCityEventsScraper(Scraper):
-
-    def lxmlize(self, url, encoding='utf-8'):
-        entry = self.urlopen(url).encode(encoding)
-        page = lxml.html.fromstring(entry)
-        page.make_links_absolute(url)
-        return page
-
-
-    def get_events(self):
-        url = "http://legistar.council.nyc.gov/Calendar.aspx"
-        page = self.lxmlize(url)
-        main = page.xpath("//table[@class='rgMasterTable']")[0]
-        rows = main.xpath(".//tr")[1:]
-        for row in rows:
-            (name, date, time, where, topic,
-             details, agenda, minutes, media) = row.xpath(".//td")
-
-            name = name.text_content().strip()  # leaving an href on the table
-            time = time.text_content().strip()
-            location = where.text_content().strip()
-            topic = topic.text_content().strip()
-
-            if "Deferred" in time:
-                continue
-
-            all_day = False
-            if time == "":
-                all_day = True
-                when = dt.datetime.strptime(date.text.strip(),
-                                            "%m/%d/%Y")
-            else:
-                when = dt.datetime.strptime("%s %s" % (date.text.strip(), time),
-                                            "%m/%d/%Y %I:%M %p")
-
-            event = Event(description=name,
-                          start=when,
-                          location=location)
-            event.add_source(url)
-
-            details = details.xpath(".//a[@href]")
-            for detail in details:
-                event.add_document(detail.text, detail.attrib['href'])
-
-            agendas = agenda.xpath(".//a[@href]")
-            for a in agendas:
-                event.add_document(a.text, a.attrib['href'])
-
-            minutes = minutes.xpath(".//a[@href]")
-            for minute in minutes:
-                event.add_document(minute.text, minute.attrib['href'])
-
-            yield event
---
+++ nyc/events.py
@@ -0,0 +1,71 @@
+# Copyright (c) Sunlight Labs, 2013, under the terms of the BSD-3 clause
+# license.
+#
+#  Contributors:
+#
+#    - Paul Tagliamonte <paultag@sunlightfoundation.com>
+
+
+from pupa.scrape import Scraper
+from larvae.event import Event
+
+import datetime as dt
+import lxml.html
+
+
+
+
+class NewYorkCityEventsScraper(Scraper):
+
+    def lxmlize(self, url, encoding='utf-8'):
+        entry = self.urlopen(url).encode(encoding)
+        page = lxml.html.fromstring(entry)
+        page.make_links_absolute(url)
+        return page
+
+
+    def get_events(self):
+        url = "http://legistar.council.nyc.gov/Calendar.aspx"
+        page = self.lxmlize(url)
+        main = page.xpath("//table[@class='rgMasterTable']")[0]
+        rows = main.xpath(".//tr")[1:]
+        for row in rows:
+            (name, date, time, where, topic,
+             details, agenda, minutes, media) = row.xpath(".//td")
+
+            name = name.text_content().strip()  # leaving an href on the table
+            time = time.text_content().strip()
+            location = where.text_content().strip()
+            topic = topic.text_content().strip()
+
+            if "Deferred" in time:
+                continue
+
+            all_day = False
+            if time == "":
+                all_day = True
+                when = dt.datetime.strptime(date.text.strip(),
+                                            "%m/%d/%Y")
+            else:
+                when = dt.datetime.strptime("%s %s" % (date.text.strip(), time),
+                                            "%m/%d/%Y %I:%M %p")
+
+            event = Event(description=name,
+                          start=when,
+                          location=location)
+            event.add_source(url)
+
+            details = details.xpath(".//a[@href]")
+            for detail in details:
+                event.add_document(detail.text, detail.attrib['href'])
+
+            agendas = agenda.xpath(".//a[@href]")
+            for a in agendas:
+                event.add_document(a.text, a.attrib['href'])
+
+            minutes = minutes.xpath(".//a[@href]")
+            for minute in minutes:
+                event.add_document(minute.text, minute.attrib['href'])
+
+            yield event
+
