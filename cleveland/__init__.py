f0ddf480a6b590ad37c66b689e8cd4ac5caf4d0c
Paul Tagliamonte <tag@pault.ag>
2013-05-28 10:00:49 -0400

    Add events base; WIP

--- cleveland/__init__.py
+++ cleveland/__init__.py
@@ -9,10 +9,18 @@ from pupa.scrape import Jurisdiction, Scraper, Legislator
 from larvae.organization import Organization
 from larvae.person import Person
 
+import datetime as dt
 import lxml.html
 import re
 
 
+# events
+CLICK_INFO = re.compile(r"CityCouncil\.popOverURL\('(?P<info_id>\d+)'\);")
+ORD_INFO = re.compile(r"Ord\. No\. (?P<ord_no>\d+-\d+)")
+AJAX_ENDPOINT = ("http://www.clevelandcitycouncil.org/plugins/NewsToolv7/"
+                 "public/calendarPopup.ashx")
+
+# people
 INFOSLUG = re.compile(r"Ward (?P<district>\d+) Council(?P<gender>.*)")
 
 
@@ -26,14 +34,19 @@ class Cleveland(Jurisdiction):
                 'terms': [{'name': '2013-2014', 'sessions': ['2013'],
                            'start_year': 2013, 'end_year': 2014
                           }],
-                'provides': ['person'],
+                'provides': ['person', 'events'],
                 'parties': [],  # No parties on the city council
                 'session_details': {'2013': {'_scraped_name': '2013'}},
                 'feature_flags': [],}
 
     def get_scraper(self, term, session, scraper_type):
-        if scraper_type == 'person':
-            return ClevelandPersonScraper
+        scrapers = {
+            "person": ClevelandPersonScraper,
+            "events": ClevelandEventScraper
+        }
+
+        if scraper_type in scrapers:
+            return scrapers[scraper_type]
 
     def scrape_session_list(self):
         return ['2013']
@@ -100,7 +113,59 @@ class ClevelandPersonScraper(Scraper):
 
             for what in scraped_info.get('committees', []):
                 p.add_committee_membership(what)
-            # XXX: Resulting in ValueError: cannot resolve id: {UUID}
-
 
             yield p
+
+
+class ClevelandEventScraper(Scraper):
+
+    def lxmlize(self, url):
+        entry = self.urlopen(url)
+        page = lxml.html.fromstring(entry)
+        page.make_links_absolute(url)
+        return page
+
+    def get_events(self):
+        page = self.lxmlize("http://www.clevelandcitycouncil.org/calendar/")
+        events = page.xpath("//ul[contains(@class, 'committee-events')]//li")
+        for event in events:
+            print(event)
+            string = event.text_content()
+
+            po = CLICK_INFO.match(event.xpath(".//span")[0].attrib['onclick'])
+            if po is None:
+                continue
+
+            poid = po.groupdict()['info_id']  # This is used to get more deetz on
+
+            popage = self.popOverUrl(poid)
+            when = dt.datetime.strptime(popage.xpath("//strong")[0].text,
+                                        "%B %d, %Y @ %I:%M %p")
+            who = popage.xpath("//h1")[0].text
+            related = []
+
+            for item in popage.xpath("//div"):
+                t = item.text
+                if t is None:
+                    continue
+
+                t = t.strip()
+                for related_entity in ORD_INFO.findall(t):
+                    related.append({
+                        "ord_no": related_entity,
+                        "what": t
+                    })
+
+            print who, when, related
+            raise Exception  # XXX: Needs work
+
+
+    def popOverUrl(self, poid):
+        data = {
+            "action": "getCalendarPopup",
+            "newsid": poid
+        }
+        page = urllib2.urlopen(AJAX_ENDPOINT, urllib.urlencode(data))
+        page = lxml.html.fromstring(page.read())
+        page.make_links_absolute(AJAX_ENDPOINT)
+        return page
