184dbd6865a2ba4d2a184f51e3abd0bc53d4a068
Paul Tagliamonte <tag@pault.ag>
2013-05-28 14:01:00 -0400

    add roswell

---
+++ roswell/__init__.py
@@ -0,0 +1,37 @@
+# Copyright (c) Sunlight Labs, 2013, under the terms of the BSD-3 clause
+# license.
+#
+#  Contributors:
+#
+#    - Paul Tagliamonte <paultag@sunlightfoundation.com>
+
+
+from pupa.scrape import Jurisdiction
+from .events import RoswellEventsScraper
+
+
+class Roswell(Jurisdiction):
+    jurisdiction_id = 'us-nm-roswell'
+
+    def get_metadata(self):
+        return {'name': 'Roswell',
+                'legislature_name': 'Roswell City Council',
+                'legislature_url': 'http://www.roswell-nm.gov/staticpages/'
+                                       'index.php/cc1-citycouncil',
+                'terms': [{'name': '2013-2014', 'sessions': ['2013'],
+                           'start_year': 2013, 'end_year': 2014
+                          }],
+                'provides': ['events'],
+                'parties': [],  # No parties on the city council
+                'session_details': {'2013': {'_scraped_name': '2013'}},
+                'feature_flags': [],}
+
+    def get_scraper(self, term, session, scraper_type):
+        bits = {
+            "events": RoswellEventsScraper
+        }
+        if scraper_type in bits:
+            return bits[scraper_type]
+
+    def scrape_session_list(self):
+        return ['2013']
---
+++ roswell/events.py
@@ -0,0 +1,78 @@
+# ~*~ encoding: utf-8 ~*~
+# Copyright (c) Sunlight Labs, 2013, under the terms of the BSD-3 clause
+# license.
+#
+#  Contributors:
+#
+#    - Paul Tagliamonte <paultag@sunlightfoundation.com>
+
+
+from pupa.scrape import Scraper
+from larvae.event import Event
+import datetime as dt
+import lxml.html
+import re
+
+CAL_PAGE = ("http://www.roswell-nm.gov/evlist/index.php?view=month&year=2013&"
+            "month=5&day=0&cal=0&cat=0")
+
+
+class RoswellEventsScraper(Scraper):
+
+    def lxmlize(self, url):
+        entry = self.urlopen(url)
+        page = lxml.html.fromstring(entry)
+        page.make_links_absolute(url)
+        return page
+
+    def get_events(self):
+        page = self.lxmlize(CAL_PAGE)
+        days = page.xpath("//table[@class='evlist_month']//td")
+        for day in days:
+            when = day.xpath(".//span[@class='date_number']//a")
+            when = when[0].text if when else None
+            if when is None:
+                continue
+            events = day.xpath(".//a[contains(@href, 'event.php')]")
+            for event in events:
+                yield self.scrape_event_page(event)
+
+    def scrape_event_page(self, event):
+        url = event.attrib['href']
+        page = self.lxmlize(url)
+        title = page.xpath("//h2[@class='evlist_header']")
+        title = title[0].text.strip() if title else None
+        if title is None:
+            return
+        if "CANCELED" in title:
+            return
+
+        info = page.xpath("//div[@style='position:relative;margin-right:40px;']")[0]
+        blocks = info.xpath(".//div")
+        ret = {}
+        for block in blocks:
+            els = block.xpath("./*")
+            if not els:
+                continue
+            le = els[0]
+
+            if le.tag != 'label':
+                continue
+
+            label, div = els
+
+            ltex = label.text_content().strip()
+            dtex = div.text_content().strip()
+            ret[ltex] = dtex
+
+        when = dt.datetime.utcnow()
+        date, start, end = (x.strip() for x in ret['When:'].split("\n"))
+        start = re.sub("^@", "", start)
+        print date, start, end
+
+        event = Event(
+            description=title,
+            location=ret['Where:'],
+            start=when,) # ret['When:'])
+        event.add_source(url)
+        yield event
