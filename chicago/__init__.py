83def29238bdf2047022f8cc4692e770c3f544ec
Paul Tagliamonte <tag@pault.ag>
2013-09-13 14:50:11 -0400

    Add Chicago base bits.

---
+++ chicago/__init__.py
@@ -0,0 +1,36 @@
+# Copyright (c) Sunlight Labs, 2013, under the terms of the BSD-3 clause
+# license.
+#
+#  Contributors:
+#
+#    - Paul Tagliamonte <paultag@sunlightfoundation.com>
+
+
+from pupa.scrape import Jurisdiction
+
+from .people import ChicagoPersonScraper
+
+
+class Chicago(Jurisdiction):
+    jurisdiction_id = 'ocd-jurisdiction/country:us/state:il/place:chicago/council'
+
+    def get_metadata(self):
+        return {'name': 'Chicago City Council',
+                'url': 'http://www.cityofchicago.org/city/en/about/council.html',
+                'terms': [{'name': '2013-2014', 'sessions': ['2013'],
+                           'start_year': 2013, 'end_year': 2014
+                          }],
+                'provides': ['people',],
+                'parties': [],  # No parties on the city council
+                'session_details': {'2013': {'_scraped_name': '2013'}},
+                'feature_flags': [],}
+
+    def get_scraper(self, term, session, scraper_type):
+        bits = {
+            "people": ChicagoPersonScraper,
+        }
+        if scraper_type in bits:
+            return bits[scraper_type]
+
+    def scrape_session_list(self):
+        return ['2013']
