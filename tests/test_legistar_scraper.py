0c1acc5ceb4ea82f36c62b7bf9843074ea0231be
Forest Gregg <fgregg@uchicago.edu>
2013-09-27 17:32:10 -0500

    Update test_legistar_scraper.py

--- tests/test_legistar_scraper.py
+++ tests/test_legistar_scraper.py
@@ -168,12 +168,12 @@ def history_row_url():
 
 @istest
 def can_get_history_detail_using_summary_row():
-  config = Config(hostname = 'phila.legistar.com',
+  config = Config(hostname = 'chicago.legistar.com',
             sponsor_links = False).defaults(DEFAULT_CONFIG)
   scraper = LegistarScraper(config)
   legislation_summary = {'URL':'https://chicago.legistar.com/LegislationDetail.aspx?ID=1450228&GUID=97689689-D0EA-47A2-8474-09B3A71C221B&Options=Advanced&Search='}
   legislation_details = scraper.expandLegislationSummary(legislation_summary)
-  history_summary = legislation_details[1][0]
+  history_summary = legislation_details[1][0]['Action Details']
 
   attrs, votes = scraper.expandHistorySummary(history_summary)
   ayes = [vote for vote in votes if vote['Vote'] == 'Ayes']
