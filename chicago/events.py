76e72fa5370471ccdd5b1744b523ea466a0bc7ca
Forest Gregg <fgregg@uchicago.edu>
2014-03-04 16:19:24 -0600

    update chicago

--- chicago/events.py
+++ chicago/events.py
@@ -1,71 +1,254 @@
-# ~*~ encoding: utf-8 ~*~
-# Copyright (c) Sunlight Labs, 2013, under the terms of the BSD-3 clause
-# license.
-#
-#  Contributors:
-#
-#    - Paul Tagliamonte <paultag@sunlightfoundation.com>
-
-
 from pupa.scrape import Scraper
 from pupa.models import Event
 import datetime as dt
 import lxml.html
+import traceback
+from collections import defaultdict
+import datetime
+import time
 
+EVENTSPAGE = "https://chicago.legistar.com/Calendar.aspx/"
 
-class ChicagoEventsScraper(Scraper):
-    def lxmlize(self, url):
-        entry = self.urlopen(url)
+class LegistarScraper(Scraper) :
+    date_format='%m/%d/%Y'
+  
+
+    def lxmlize(self, url, payload=None):
+        entry = self.urlopen(url, 'POST', payload)
         page = lxml.html.fromstring(entry)
         page.make_links_absolute(url)
         return page
 
-    def get_events(self):
-        if self.session != self.get_current_session():
-            raise Exception("Can't do that, dude")
 
-        url = "http://chicago.legistar.com/Calendar.aspx/"
-        page = self.lxmlize(url)
-        main = page.xpath("//table[@class='rgMasterTable']")[0]
-        rows = main.xpath(".//tr")[1:]
+    def pages(self, url, payload=None) :
+        page = self.lxmlize(url, payload)
+        yield page
+
+        next_page = page.xpath("//a[@class='rgCurrentPage']/following-sibling::a[1]")
+
+        while len(next_page) > 0 : 
+
+            payload = self.sessionSecrets(page)
+            event_target = next_page[0].attrib['href'].split("'")[1]
+
+            payload['__EVENTTARGET'] = event_target
+
+            page = self.lxmlize(url, payload)
+
+            yield page
+
+            next_page = page.xpath("//a[@class='rgCurrentPage']/following-sibling::a[1]")
+
+
+    def councilMembers(self, follow_links=True) :
+        for page in self.pages(MEMBERLIST) :
+            table = page.xpath("//table[@id='ctl00_ContentPlaceHolder1_gridPeople_ctl00']")[0]
+
+            for councilman, headers, row in self.parseDataTable(table):
+
+                if follow_links and type(councilman['Person Name']) == dict :
+                    detail_url = councilman['Person Name']['url']
+                    councilman_details = self.lxmlize(detail_url)
+                    img = councilman_details.xpath("//img[@id='ctl00_ContentPlaceHolder1_imgPhoto']")
+                    if img :
+                        councilman['Photo'] = img[0].get('src')
+
+                    committee_table = councilman_details.xpath("//table[@id='ctl00_ContentPlaceHolder1_gridDepartments_ctl00']")[0]
+                    
+                    committees = self.parseDataTable(committee_table)
+
+                    yield councilman, committees
+
+                else :
+                    yield councilman
+
+    def parseDataTable(self, table):
+        """
+        Legistar uses the same kind of data table in a number of
+        places. This will return a list of dictionaries using the
+        table headers as keys.
+        """
+        headers = table.xpath('.//th')
+        rows = table.xpath(".//tr[starts-with(@id, 'ctl00_ContentPlaceHolder1_')]")
+
+
+        keys = {}
+        for index, header in enumerate(headers):
+            keys[index] = header.text_content().replace('&nbsp;', ' ').strip()
+
         for row in rows:
-            if "No records were found." in row.text_content():
-                self.warning("Hum. They don't seem to have events?")
-                continue
+          try:
+            data = defaultdict(lambda : None)
 
-            (name, date, _, time, where, details, notice,
-             agenda, summary, video) = row.xpath(".//td")
-            # _ nom's the image next to the date on the page.
+            for index, field in enumerate(row.xpath("./td")):
+                key = keys[index]
+                value = field.text_content().replace('&nbsp;', ' ').strip()
 
-            name = name.text_content().strip()  # leaving an href on the table
-            time = time.text_content().strip()
-            location = where.text_content().strip()
+                try:
+                    value = datetime.datetime.strptime(value, self.date_format)
+                except ValueError:
+                    pass
 
-            if "Deferred" in time:
-                continue
+                # Is it a link?
+                address = None
+                link = field.xpath('.//a')
+                if len(link) > 0 :
+                    address = self._get_link_address(link[0])
+                if address is not None:
+                    value = {'label': value, 'url': address}
+                    
+                data[key] = value
 
-            all_day = False
-            if time == "":
-                all_day = True
-                when = dt.datetime.strptime(date.text.strip(),
-                                            "%m/%d/%Y")
-            else:
-                when = dt.datetime.strptime("%s %s" % (date.text.strip(), time),
-                                            "%m/%d/%Y %I:%M %p")
+            yield data, keys, row
 
-            event = Event(name=name,
+          except Exception as e:
+            print 'Problem parsing row:'
+            print row
+            print traceback.format_exc()
+            raise e
+
+    def _get_link_address(self, link):
+        # If the link doesn't start with a #, then it'll send the browser
+        # somewhere, and we should use the href value directly.
+        href = link.get('href')
+        if href is not None and href != self.base_url :
+          return href
+
+        # If it does start with a hash, then it causes some sort of action
+        # and we should check the onclick handler.
+        else:
+          onclick = link.get('onclick')
+          if onclick is not None and 'open(' in onclick :
+            return self.base_url + onclick.split("'")[1]
+
+        # Otherwise, we don't know how to find the address.
+        return None
+
+    def sessionSecrets(self, page) :
+        
+        payload = {}
+        #print page.xpath("//input[@name='__EVENTARGUMENT']/@value")
+        payload['__EVENTARGUMENT'] = None
+        payload['__VIEWSTATE'] = page.xpath("//input[@name='__VIEWSTATE']/@value")[0]
+        payload['__VSTATE'] = page.xpath("//input[@name='__VSTATE']/@value")[0]
+
+        payload['__EVENTVALIDATION'] = page.xpath("//input[@name='__EVENTVALIDATION']/@value")[0]
+
+        return(payload)
+
+
+    def eventPages(self, event_url, search_type='all') :
+
+        page = self.lxmlize(event_url) 
+
+        if search_type == 'all' :
+            payload = self.sessionSecrets(page)
+
+            payload['ctl00$ContentPlaceHolder1$lstYears'] = 'All Years'
+            payload['ctl00_ContentPlaceHolder1_lstYears_ClientState'] = '{"logEntries":[],"value":"All","text":"All Years","enabled":true,"checkedIndices":[],"checkedItemsTextOverflows":false}'
+
+
+            payload['__EVENTTARGET'] = 'ctl00$ContentPlaceHolder1$lstYears'
+
+            #print payload
+
+            return self.pages(event_url, payload)
+
+        else :
+            return self.pages(page)
+
+
+
+class ChicagoEventsScraper(LegistarScraper):
+    base_url = "https://chicago.legistar.com/"
+
+    def get_events(self):
+        for page in self.eventPages(EVENTSPAGE) :
+            events_table = page.xpath("//table[@class='rgMasterTable']")[0]
+            for events, headers, rows in self.parseDataTable(events_table) :
+                location_string = events[u'Meeting\xa0Location']
+                location_list = location_string.split('--')
+                location = ', '.join(location_list[0:2])
+                
+                status_string = location_list[-1].split('Chicago, Illinois')
+                if len(status_string) > 1 and status_string[1] :
+                    status = status_string[1].lower()
+                    if status not in ['cancelled', 'tentative', 'confirmed', 'passed'] :
+                        print status
+                        status = 'confirmed'
+                else :
+                    status = 'confirmed'
+
+                    
+                
+                when = events[u'Meeting\xa0Date']
+                time_string = events[u'Meeting\xa0Time']
+                event_time = datetime.datetime.strptime(time_string, 
+                                                        "%I:%M %p")
+                when = when.replace(hour=event_time.hour)
+
+                e = Event(name=events["Name"]["label"],
                           session=self.session,
                           when=when,
-                          location=location)
-            event.add_source(url)
+                          location=location,
+                          status=status)
+                e.add_source(EVENTSPAGE)
+                if events['Video'] != u'Not\xa0available' :
+                    print events['Video']
+                
+                #e.add_media_link(
+                yield e
+                
 
-            agendas = agenda.xpath(".//a[@href]")
-            for a in agendas:
-                event.add_link(a.text, a.attrib['href'])
+                
 
-            summary = summary.xpath(".//a[@href]")
-            for minute in summary:
-                event.add_link(minute.text, minute.attrib['href'])
+        #     event = Event(name=name,
+        #                   session=self.session,
+        #                   when=when,
+        #                   location=location)
+                          
+                          
+                #print events
+        
+        # main = 
+        # rows = main.xpath(".//tr")[1:]
+        # for row in rows:
+        #     if "No records were found." in row.text_content():
+        #         self.warning("Hum. They don't seem to have events?")
+        #         continue
 
-            yield event
+        #     (name, date, _, time, where, details, notice,
+        #      agenda, summary, video) = row.xpath(".//td")
+        #     # _ nom's the image next to the date on the page.
 
+        #     name = name.text_content().strip() # leaving an href on the table
+        #     time = time.text_content().strip()
+        #     location = where.text_content().strip()
+
+        #     if "Deferred" in time:
+        #         continue
+
+        #     all_day = False
+        #     if time == "":
+        #         all_day = True
+        #         when = dt.datetime.strptime(date.text.strip(),
+        #                                     "%m/%d/%Y")
+        #     else:
+        #         when = dt.datetime.strptime("%s %s" % (date.text.strip(), time),
+        #                                     "%m/%d/%Y %I:%M %p")
+
+        #     event = Event(name=name,
+        #                   session=self.session,
+        #                   when=when,
+        #                   location=location)
+        #     event.add_source(url)
+
+        #     agendas = agenda.xpath(".//a[@href]")
+        #     for a in agendas:
+        #         event.add_link(a.text, a.attrib['href'])
+
+        #     summary = summary.xpath(".//a[@href]")
+        #     for minute in summary:
+        #         event.add_link(minute.text, minute.attrib['href'])
+
+        #     yield event
--- chicago/people.py
+++ chicago/people.py
@@ -1,80 +1,185 @@
-# Copyright (c) Sunlight Labs, 2013, under the terms of the BSD-3 clause
-# license.
-#
-#  Contributors:
-#
-#    - Paul Tagliamonte <paultag@sunlightfoundation.com>
-
-
-from pupa.scrape import Scraper, Legislator, Committee
-
-from collections import defaultdict
+from pupa.scrape import Scraper
+from pupa.scrape.helpers import Legislator, Membership, Organization
 import lxml.html
+import datetime
+import traceback
+from collections import defaultdict
+import logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-MEMBER_LIST = "http://www.cityofchicago.org/city/en/about/wards.html"
+MEMBERLIST = 'https://chicago.legistar.com/People.aspx'
 
+class LegistarScraper(Scraper) :
+    date_format='%m/%d/%Y'
+  
 
-class ChicagoPersonScraper(Scraper):
-
-    def lxmlize(self, url):
-        entry = self.urlopen(url)
+    def lxmlize(self, url, payload=None):
+        entry = self.urlopen(url, 'POST', payload)
         page = lxml.html.fromstring(entry)
         page.make_links_absolute(url)
         return page
 
+
+    def pages(self, url, payload=None) :
+        page = self.lxmlize(url, payload)
+        yield page
+
+        next_page = page.xpath("//a[@class='rgCurrentPage']/following-sibling::a[1]")
+
+        while next_page : 
+
+            event_target = next_page.attrib['href'].split("'")[1]
+            br.select_form('aspnetForm')
+            data = self._data(br.form, event_target)
+
+            del data['ctl00$ContentPlaceHolder1$gridPeople$ctl00$ctl02$ctl01$ctl01']
+            # print data
+            data = urllib.urlencode(data)
+            page = lxmlize(url, payload)
+
+            yield page
+
+            next_page = page.xpath("//a[@class='rgCurrentPage']/following-sibling::a[1]")
+
+
+    def councilMembers(self, follow_links=True) :
+        for page in self.pages(MEMBERLIST) :
+            table = page.xpath("//table[@id='ctl00_ContentPlaceHolder1_gridPeople_ctl00']")[0]
+
+            for councilman, headers, row in self.parseDataTable(table):
+
+                if follow_links and type(councilman['Person Name']) == dict :
+                    detail_url = councilman['Person Name']['url']
+                    councilman_details = self.lxmlize(detail_url)
+                    img = councilman_details.xpath("//img[@id='ctl00_ContentPlaceHolder1_imgPhoto']")
+                    if img :
+                        councilman['Photo'] = img[0].get('src')
+
+                    committee_table = councilman_details.xpath("//table[@id='ctl00_ContentPlaceHolder1_gridDepartments_ctl00']")[0]
+                    
+                    committees = self.parseDataTable(committee_table)
+
+                    yield councilman, committees
+
+                else :
+                    yield councilman
+
+                
+
+    def _get_link_address(self, link_soup):
+        # If the link doesn't start with a #, then it'll send the browser
+        # somewhere, and we should use the href value directly.
+        href = link_soup.get('href')
+        if href is not None and not href.startswith('#'):
+          return href
+
+        # If it does start with a hash, then it causes some sort of action
+        # and we should check the onclick handler.
+        else:
+          onclick = link_soup.get('onclick')
+          if onclick is not None and onclick.startswith("radopen('"):
+            return onclick.split("'")[1]
+
+        # Otherwise, we don't know how to find the address.
+        return None
+    
+    def parseDataTable(self, table):
+        """
+        Legistar uses the same kind of data table in a number of
+        places. This will return a list of dictionaries using the
+        table headers as keys.
+        """
+        headers = table.xpath('.//th')
+        rows = table.xpath(".//tr[starts-with(@id, 'ctl00_ContentPlaceHolder1_')]")
+
+
+        keys = {}
+        for index, header in enumerate(headers):
+            keys[index] = header.text_content().replace('&nbsp;', ' ').strip()
+
+        for row in rows:
+          try:
+            data = defaultdict(lambda : None)
+
+            for index, field in enumerate(row.xpath("./td")):
+                key = keys[index]
+                value = field.text_content().replace('&nbsp;', ' ').strip()
+
+                try:
+                    value = datetime.datetime.strptime(value, self.date_format)
+                except ValueError:
+                    pass
+
+                # Is it a link?
+                address = None
+                link = field.getchildren()[0].find('a')
+                if link is not None:
+                    address = self._get_link_address(link)
+                if address is not None:
+                    value = {'label': value, 'url': address}
+                    
+                data[key] = value
+
+            yield data, keys, row
+
+          except Exception as e:
+            print 'Problem parsing row:'
+            print row
+            print traceback.format_exc()
+            raise e
+    
+
+
+
+class ChicagoPersonScraper(LegistarScraper):
+    
+
+    
     def get_people(self):
-        yield self.bos_scrape_people()
+        for councilman, committees in self.councilMembers() :
+            contact_types = {
+                "City Hall Office": ("address", "City Hall Office"),
+                "City Hall Phone": ("phone", "City Hall Phone"),
+                "Ward Office Phone": ("phone", "Ward Office Phone"),
+                "Ward Office Address": ("address", "Ward Office Address"),
+                "Fax": ("fax", "Fax")
+            }
+            
+            contacts = []
+            for contact_type, (_type, note) in contact_types.items () :
+                if councilman[contact_type] : 
+                    contacts.append({"type": _type,
+                                     "value": councilman[contact_type],
+                                     "note": note})
 
-    def scrape_ward(self, el):
-        url = el.attrib['href']
-        page = self.lxmlize(url)
-        name = page.xpath("//div[@id='content-content']/h3")[0].text_content()
-        badthings = [
-            "Alderman"
-        ]
-        for thing in badthings:
-            if name.startswith(thing):
-                name = name[len(thing):].strip()
+            if councilman["E-mail"] : 
+                contacts.append({"type" : "email",
+                                 "value" : councilman['E-mail']['label'],
+                                 'note' : 'E-mail'})
 
-        district = page.xpath("//h1[@class='page-heading']/text()")[0]
-        leg = Legislator(name=name, post_id=district)
-        leg.add_source(url)
 
-        type_types = {
-            "City Hall Office:": ("address", "City Hall Office"),
-            "City Hall Phone:": ("phone", "City Hall Phone"),
-            "Phone:": ("phone", "Personal Phone"),
-            "Office:": ("address", "Personal Office"),
-            "Fax:": ("fax", "Fax"),
-            "Fax": ("fax", "Fax"),
-        }
+            p = Legislator(councilman['Person Name']['label'],
+                           post_id = councilman['Ward/Office'],
+                           image=councilman['Photo'],
+                           contact_details = contacts)
 
-        for row in page.xpath("//table//tr"):
-            type_, val = (x.text_content().strip() for x in row.xpath("./td"))
-            if val == "":
-                continue
 
-            types = [type_]
-            vals = [val]
+            if councilman['Website'] :
+                p.add_link('homepage', councilman['Website']['url'])
+            p.add_source(MEMBERLIST)
 
-            if "\n" in type_:
-                if "\n" in val:
-                    types = type_.split("\n")
-                    vals = val.split("\n")
-                else:
-                    continue
+            for committee, _, _ in committees :
+                print committee
+                if committee['Legislative Body']['label'] :
+                    if committee['Legislative Body']['label'] not in ('City Council', 'Office of the Mayor') :
+                        p.add_committee_membership(committee['Legislative Body']['label'], 
+                                                   role= committee["Title"])
+                        
 
-            for type_ in types:
-                for val in vals:
-                    ctype, note = type_types[type_]
-                    leg.add_contact(ctype, val, note)
 
-        return leg
+            yield p
 
-    def bos_scrape_people(self):
-        page = self.lxmlize(MEMBER_LIST)
-        wards = page.xpath("//div[@id='content-content']/h4/"
-                           "a[contains(@href, 'city/en/about/wards')]")
-        for ward in wards:
-            yield self.scrape_ward(ward)
+
+
