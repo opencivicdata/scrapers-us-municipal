0269ebd54830acc8fec79a6ca752243772dc66d4
twneale <twneale@gmail.com>
2014-05-22 15:59:09 -0400

    Added legistar person scraper

--- nyc/__init__.py
+++ nyc/__init__.py
@@ -1,16 +1,17 @@
 from pupa.scrape import Jurisdiction
 
-from .events import NewYorkCityEventsScraper
+# from .events import NewYorkCityEventsScraper
 from .people import NewYorkCityPersonScraper
 
 
 class NewYorkCity(Jurisdiction):
-    jurisdiction_id = 'ocd-jurisdiction/country:us/state:ny/place:new_york/council'
+    division_id = 'ocd-division/country:us/state:ny/place:new_york'
+    jurisdiction_id = 'ocd-jurisdiction/country:us/state:ny/place:new_york/government'
     name = 'New York City Council'
     url = 'http://council.nyc.gov/'
     parties = [ {'name': 'Democratic' }, {'name': 'Republican' } ]
 
     scrapers = {
         "people": NewYorkCityPersonScraper,
-        "events": NewYorkCityEventsScraper
+        # "events": NewYorkCityEventsScraper
     }
