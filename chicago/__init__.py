01d9b734ec486a2ecc90e0e621f6953edc421519
twneale <twneale@gmail.com>
2014-05-27 10:40:15 -0400

    Chicago people scrape

--- chicago/__init__.py
+++ chicago/__init__.py
@@ -7,6 +7,7 @@ from .bills import ChicagoBillScraper
 
 class Chicago(Jurisdiction):
     jurisdiction_id = 'ocd-jurisdiction/country:us/state:il/place:chicago/council'
+    division_id = 'ocd-jurisdiction/country:us/state:il/place:chicago'
     name = 'Chicago City Council'
     url =  'https://chicago.legistar.com/'
     parties = [ {'name': 'Democrats' } ]
