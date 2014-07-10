fc95c2e432ba6e0ec10a6c38729cc6826ebde85a
twneale <twneale@gmail.com>
2014-07-10 17:20:41 -0400

    Update NYC pupa config with legistar changes.

--- nyc/__init__.py
+++ nyc/__init__.py
@@ -1,17 +1,36 @@
 from pupa.scrape import Jurisdiction
 
-# from .events import NewYorkCityEventsScraper
-from legistar.ext.pupa import LegistarPeopleScraper
+from legistar.ext.pupa import (
+    LegistarPeopleScraper, LegistarEventsScraper, LegistarBillsScraper,
+    generate_orgs)
 
 
 class NewYorkCity(Jurisdiction):
+    classification = 'government'
     division_id = 'ocd-division/country:us/state:ny/place:new_york'
-    jurisdiction_id = 'ocd-jurisdiction/country:us/state:ny/place:new_york/government'
-    name = 'New York City Council'
+    name = 'New York City'
     url = 'http://council.nyc.gov/'
-    parties = [ {'name': 'Democratic' }, {'name': 'Republican' } ]
+    parties = [{'name': 'Democratic' }, {'name': 'Republican' }]
+
+    legislative_sessions = [
+        {'name': '2014', 'identifier': '2014'},
+        ]
 
     scrapers = {
         "people": LegistarPeopleScraper,
-        # "events": NewYorkCityEventsScraper
+        "events": LegistarEventsScraper,
+        "bills": LegistarBillsScraper,
     }
+
+    def get_organizations(self):
+        for org in generate_orgs(pupa_jurisdiction=self):
+            if org.classification != 'legislature':
+                yield org
+                continue
+
+            # NYC Council has 51 districts, each with the integer as an id.
+            for district in range(1, 52):
+                org.add_post(
+                    label='District %d' % district, role='Council Member')
+
+            yield org
