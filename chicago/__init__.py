761a938c103764d68ba6824c602fa182ddb621ab
Forest Gregg <fgregg@uchicago.edu>
2014-09-16 10:49:35 -0500

    chicago people seems to be working with new pupa

--- chicago/__init__.py
+++ chicago/__init__.py
@@ -1,4 +1,4 @@
-from pupa.scrape import Jurisdiction
+from pupa.scrape import Jurisdiction, Organization
 
 from .people import ChicagoPersonScraper
 from .events import ChicagoEventsScraper
@@ -8,6 +8,7 @@ from .bills import ChicagoBillScraper
 class Chicago(Jurisdiction):
     jurisdiction_id = 'ocd-jurisdiction/country:us/state:il/place:chicago/council'
     division_id = 'ocd-jurisdiction/country:us/state:il/place:chicago'
+    classification='council'
     name = 'Chicago City Council'
     url =  'https://chicago.legistar.com/'
     parties = [ {'name': 'Democrats' } ]
@@ -17,3 +18,8 @@ class Chicago(Jurisdiction):
         "events": ChicagoEventsScraper,
         "bills": ChicagoBillScraper,
     }
+
+
+    def get_organizations(self):
+        org = Organization(name="Chicago City Council", classification="legislature")
+        yield org
