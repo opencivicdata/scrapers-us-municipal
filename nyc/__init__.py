fae36a04cdf5879f1cf5a23280d2d9761c164b61
Forest Gregg <fgregg@uchicago.edu>
2015-06-15 12:15:09 -0500

    nyc people working

--- nyc/__init__.py
+++ nyc/__init__.py
@@ -1,32 +1,25 @@
 # encoding=utf-8
 from pupa.scrape import Jurisdiction, Organization
-from .bills import NycBillScraper
-from .events import NycEventScraper
-from .people import NycPersonScraper
+from .people import NYCPersonScraper
 
+class NYC(Jurisdiction):
+    classification = 'government'
+    division_id = 'ocd-division/country:us/state:ny/place:new_york'
+    name = 'New York City'
+    timezone = 'America/New_York'
+    url = 'http://nyc.gov'
 
-class Nyc(Jurisdiction):
-    division_id = "ocd-division/country:us/state:ny/place:new_york"
-    classification = "government"
-    name = "City of New York"
-    url = "council.nyc.gov"
-    scrapers = {
-        "bills": NycBillScraper,
-        "events": NycEventScraper,
-        "people": NycPersonScraper,
-    }
+    parties = [
+        {'name': 'Democratic'},
+        {'name': 'Republican'}
+    ]
+    scrapers = {'people': NYCPersonScraper}
 
     def get_organizations(self):
-        #REQUIRED: define an organization using this format
-        #where org_name is something like Seattle City Council
-        #and classification is described here:
-        org = Organization(name="org_name", classification="legislature")
+        council = Organization('New York City Council', classification='legislature')
+        for x in range(1,52):
+            council.add_post(str(x), role='Council Member')
+        yield council
 
-        #OPTIONAL: add posts to your organizaion using thi format,
-        #where label is a human-readable description of the post (eg "Ward 8 conucilmember")
-        #and role is the positoin type (eg conucilmember, alderman, mayor...)
-        #skip entirely if you're not writing a people scraper.
-        org.add_post(label="position_description", role="position_type")
+    LEGISTAR_ROOT_URL = 'http://legistar.council.nyc.gov/'
 
-        #REQUIIRED: yield the organization
-        yield org
--- nyc/people.py
+++ nyc/people.py
@@ -1,9 +1,61 @@
-from pupa.scrape import Scraper
-from pupa.scrape import Person
+import re
+
+from pupa.scrape import Person, Organization
+from legistar.people import LegistarPersonScraper
 
 
-class NycPersonScraper(Scraper):
+
+class NYCPersonScraper(LegistarPersonScraper):
+    MEMBERLIST = 'http://legistar.council.nyc.gov/People.aspx'
 
     def scrape(self):
-        # needs to be implemented
-        pass
+        non_committees = {'City Council'}
+        committee_d = {}
+
+        for councilman, committees in self.councilMembers() :
+            district, role, _ = councilman['Notes'].split(' - ', maxsplit=2)
+
+            if ('Democrat' in councilman['Notes'] 
+                or 'Democratic' in councilman['Notes']) :
+                party = 'Democratic'
+            elif 'Republican' in councilman ['Notes'] :
+                party = 'Republican'
+            else :
+                party = None
+            
+            
+
+            p = Person(councilman['Person Name']['label'],
+                       district=district,
+                       primary_org="legislature",
+                       role=role)
+
+            if councilman['Photo'] :
+                p.image = councilman['Photo']
+
+            if councilman["E-mail"]:
+                p.add_contact_detail(type="email",
+                                     value=councilman['E-mail'],
+                                     note='E-mail')
+
+            if councilman['Website']:
+                p.add_link(councilman['Website']['url'])
+                
+            p.add_source(councilman['Person Name']['url'])
+
+            for committee, _, _ in committees:
+                committee_name = committee['Department Name']['label']
+                if committee_name and committee_name not in non_committees:
+                    o = committee_d.get(committee_name, None)
+                    if o is None:
+                        o = Organization(committee_name,
+                                         classification='committee')
+                        o.add_source("http://legistar.council.nyc.gov/Departments.aspx")
+                        committee_d[committee_name] = o
+
+                    membership = o.add_member(p, role=committee["Title"])
+                    membership.start_date = self.mdY2Ymd(committee["Start Date"])
+            yield p
+
+        for o in committee_d.values() :
+            yield o
