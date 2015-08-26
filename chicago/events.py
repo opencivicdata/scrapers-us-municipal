a44c7e014c39e31fffc0f0709273a562f120fee8
Forest Gregg <fgregg@uchicago.edu>
2015-08-26 17:29:28 -0500

    chicago events uses legistar package

--- chicago/events.py
+++ chicago/events.py
@@ -1,164 +1,126 @@
 from pupa.scrape import Event
-from .legistar import LegistarScraper
+from legistar.events import LegistarEventsScraper
 from collections import defaultdict
 import datetime
 import lxml
 import lxml.etree
 import pytz
 
-EVENTSPAGE = "https://chicago.legistar.com/Calendar.aspx/"
+class ChicagoEventsScraper(LegistarEventsScraper) :
+    EVENTSPAGE = "https://chicago.legistar.com/Calendar.aspx"
+    BASE_URL = "https://chicago.legistar.com/"
+    TIMEZONE = "US/Central"
 
-class ChicagoEventsScraper(LegistarScraper):
-    base_url = "https://chicago.legistar.com/"
-    timezone = "US/Central"
+    def scrape(self):
+        for event, agenda in self.events() :
 
-    def eventPages(self, event_url, search_type='all') :
+            description = None
 
-        page = self.lxmlize(event_url)
+            location_string = event[u'Meeting Location']
 
-        if search_type == 'all' :
-            payload = self.sessionSecrets(page)
+            location_list = location_string.split('--', 2)
+            location = ', '.join(location_list[0:2])
+            if not location :
+                continue
 
-            payload['ctl00$ContentPlaceHolder1$lstYears'] = 'All Years'
-            payload['ctl00_ContentPlaceHolder1_lstYears_ClientState'] = '{"logEntries":[],"value":"All","text":"All Years","enabled":true,"checkedIndices":[],"checkedItemsTextOverflows":false}'
+            when = self.toTime(event[u'Meeting Date'])
+            time_string = event[u'Meeting Time']
+            if time_string :
+                event_time = datetime.datetime.strptime(time_string,
+                                                        "%I:%M %p")
+                when = when.replace(hour=event_time.hour)
 
-
-            payload['__EVENTTARGET'] = 'ctl00$ContentPlaceHolder1$lstYears'
-
-            return self.pages(event_url, payload)
-
-        else :
-            return self.pages(page)
-
-
-    def scrape(self, follow_links=True):
-        for page in self.eventPages(EVENTSPAGE):
-            events_table = page.xpath("//table[@class='rgMasterTable']")[0]
-            for events, headers, rows in self.parseDataTable(events_table) :
-                description = None
-                if follow_links and type(events['Meeting\xa0Details']) == dict :
-                    detail_url = events['Meeting\xa0Details']['url']
-                    meeting_details = self.lxmlize(detail_url)
-
-                    agenda_table = meeting_details.xpath(
-                        "//table[@id='ctl00_ContentPlaceHolder1_gridMain_ctl00']")[0]
-                    agenda = self.parseDataTable(agenda_table)
-
-                else :
-                    meeting_details = False
-                    
-                location_string = events[u'Meeting\xa0Location']
-                location_list = location_string.split('--', 2)
-                location = ', '.join(location_list[0:2])
-                if not location :
-                    continue
-
-                when = self.toTime(events[u'Meeting\xa0Date'])
-                time_string = events[u'Meeting\xa0Time']
-                if time_string :
-                    event_time = datetime.datetime.strptime(time_string,
-                                                            "%I:%M %p")
-                    when = when.replace(hour=event_time.hour)
-                    
-                status_string = location_list[-1].split('Chicago, Illinois')
-                if len(status_string) > 1 and status_string[1] :
-                    status_text = status_string[1].lower()
-                    if any(phrase in status_text 
-                           for phrase in ('rescheduled to',
-                                          'postponed to',
-                                          'reconvened to',
-                                          'rescheduled to',
-                                          'meeting recessed',
-                                          'recessed meeting',
-                                          'postponed to',
-                                          'recessed until',
-                                          'deferred',
-                                          'time change',
-                                          'date change',
-                                          'recessed meeting - reconvene',
-                                          'cancelled',
-                                          'new date and time',
-                                          'rescheduled indefinitely',
-                                          'rescheduled for',)) :
-                        status = 'cancelled'
-                    elif status_text in ('rescheduled', 'recessed') :
-                        status = 'cancelled'
-                    elif status_text in ('meeting reconvened',
-                                         'reconvened meeting',
-                                         'recessed meeting',
-                                         'reconvene meeting',
-                                         'rescheduled hearing',
-                                         'rescheduled meeting',) :
-                        status = confirmedOrPassed(when)
-                    elif status_text in ('amended notice of meeting',
-                                         'room change',
-                                         'amended notice',
-                                         'change of location',
-                                         'revised - meeting date and time') :
-                        status = confirmedOrPassed(when)
-                    elif 'room' in status_text :
-                        location = status_string[1] + ', ' + location
-                    elif status_text in ('wrong meeting date',) :
-                        continue
-                    else :
-                        print(status_text)
-                        description = status_string[1]
-                        status = confirmedOrPassed(when)
-                else :
+            status_string = location_list[-1].split('Chicago, Illinois')
+            if len(status_string) > 1 and status_string[1] :
+                status_text = status_string[1].lower()
+                if any(phrase in status_text 
+                       for phrase in ('rescheduled to',
+                                      'postponed to',
+                                      'reconvened to',
+                                      'rescheduled to',
+                                      'meeting recessed',
+                                      'recessed meeting',
+                                      'postponed to',
+                                      'recessed until',
+                                      'deferred',
+                                      'time change',
+                                      'date change',
+                                      'recessed meeting - reconvene',
+                                      'cancelled',
+                                      'new date and time',
+                                      'rescheduled indefinitely',
+                                      'rescheduled for',)) :
+                    status = 'cancelled'
+                elif status_text in ('rescheduled', 'recessed') :
+                    status = 'cancelled'
+                elif status_text in ('meeting reconvened',
+                                     'reconvened meeting',
+                                     'recessed meeting',
+                                     'reconvene meeting',
+                                     'rescheduled hearing',
+                                     'rescheduled meeting',) :
                     status = confirmedOrPassed(when)
-                            
-
-                if description :
-                    e = Event(name=events["Name"]["label"],
-                              start_time=when,
-                              description=description,
-                              timezone='US/Central',
-                              location_name=location,
-                              status=status)
+                elif status_text in ('amended notice of meeting',
+                                     'room change',
+                                     'amended notice',
+                                     'change of location',
+                                     'revised - meeting date and time') :
+                    status = confirmedOrPassed(when)
+                elif 'room' in status_text :
+                    location = status_string[1] + ', ' + location
+                elif status_text in ('wrong meeting date',) :
+                    continue
                 else :
-                    e = Event(name=events["Name"]["label"],
-                              start_time=when,
-                              timezone='US/Central',
-                              location_name=location,
-                              status=status)
+                    print(status_text)
+                    description = status_string[1]
+                    status = confirmedOrPassed(when)
+            else :
+                status = confirmedOrPassed(when)
 
 
-                if events['Video'] != 'Not\xa0available' : 
-                    e.add_media_link(note='Recording',
-                                     url = events['Video']['url'],
-                                     type="recording",
-                                     media_type = 'text/html')
+            if description :
+                e = Event(name=event["Name"]["label"],
+                          start_time=when,
+                          description=description,
+                          timezone='US/Central',
+                          location_name=location,
+                          status=status)
+            else :
+                e = Event(name=event["Name"]["label"],
+                          start_time=when,
+                          timezone='US/Central',
+                          location_name=location,
+                          status=status)
 
-                addDocs(e, events, 'Agenda')
-                addDocs(e, events, 'Notice')
-                addDocs(e, events, 'Transcript')
-                addDocs(e, events, 'Summary')
 
-                e.add_participant(name=events["Name"]["label"],
-                                  type="organization")
+            if event['Video'] != 'Not\xa0available' : 
+                e.add_media_link(note='Recording',
+                                 url = event['Video']['url'],
+                                 type="recording",
+                                 media_type = 'text/html')
 
-                if meeting_details :
-                    e.add_source(detail_url)
+            self.addDocs(e, event, 'Agenda')
+            self.addDocs(e, event, 'Notice')
+            self.addDocs(e, event, 'Transcript')
+            self.addDocs(e, event, 'Summary')
 
-                    for item, _, _ in agenda :
-                        agenda_item = e.add_agenda_item(item["Title"])
-                        if item["Record #"] :
-                            agenda_item.add_bill(item["Record #"]['label'])
+            e.add_participant(name=event["Name"]["label"],
+                              type="organization")
 
-                else :
-                    e.add_source(EVENTSPAGE)
+            if agenda :
+                e.add_source(event['Name']['url'])
 
-                yield e
+                
+                for item, _, _ in agenda :
+                    agenda_item = e.add_agenda_item(item["Title"])
+                    if item["Record #"] :
+                        agenda_item.add_bill(item["Record #"]['label'])
 
-def addDocs(e, events, doc_type) :
-    try :
-        if events[doc_type] != 'Not\xa0available' : 
-            e.add_document(note= events[doc_type]['label'],
-                           url = events[doc_type]['url'],
-                           media_type="application/pdf")
-    except ValueError :
-        pass
-        
+            else :
+                e.add_source(self.EVENTSPAGE)
+
+            yield e
+
 def confirmedOrPassed(when) :
     if datetime.datetime.utcnow().replace(tzinfo = pytz.utc) > when :
         status = 'confirmed'
