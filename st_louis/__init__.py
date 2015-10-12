fcbdfeb70b7c876629bcb7db90ad1fc8b1e29d90
jesseilev <jesseilev@gmail.com>
2015-10-12 00:26:57 -0500

    implement scraper, can scrape aldermen along with image, phone, and membership in Board of Ald

--- st_louis/__init__.py
+++ st_louis/__init__.py
@@ -1,28 +1,36 @@
 # encoding=utf-8
 from pupa.scrape import Jurisdiction, Organization
-from .people import St_LouisPersonScraper
+from .people import StLouisPersonScraper
 
 
-class St_Louis(Jurisdiction):
+class StLouis(Jurisdiction):
     division_id = "ocd-division/country:us/state:mo/place:st_louis"
+    jurisdiction_id = "ocd-jurisdiction/country:us/state:mo/place:st_louis"
     classification = "legislature"
     name = "St. Louis city Board of Aldermen"
     url = "https://www.stlouis-mo.gov/government/departments/aldermen/"
     scrapers = {
-        "people": St_LouisPersonScraper,
+        "people": StLouisPersonScraper,
     }
 
     def get_organizations(self):
+
+        WARD_COUNT = 28
+
         #REQUIRED: define an organization using this format
         #where org_name is something like Seattle City Council
         #and classification is described here:
-        org = Organization(name="org_name", classification="legislature")
+        org = Organization(name="St Louis Board of Aldermen", 
+                           classification="legislature")
 
         # OPTIONAL: add posts to your organizaion using thi format,
         # where label is a human-readable description of the post (eg "Ward 8 councilmember")
         # and role is the positoin type (eg conucilmember, alderman, mayor...)
         # skip entirely if you're not writing a people scraper.
         org.add_post(label="position_description", role="position_type")
+        for ward_num in range(1, WARD_COUNT + 1):
+            org.add_post(label="Ward {} Alderman".format(ward_num),
+                         role="Alderman")
 
         #REQUIIRED: yield the organization
         yield org
--- st_louis/people.py
+++ st_louis/people.py
@@ -1,9 +1,59 @@
 from pupa.scrape import Scraper
 from pupa.scrape import Person
+from lxml import html
+import requests
 
+BASE_URL = "https://www.stlouis-mo.gov"
+ALDERMEN_HOME = BASE_URL + "/government/departments/aldermen"
 
-class St_LouisPersonScraper(Scraper):
+# FIXME duplication from StLouis Jurisdiction class in __init__.py
+WARD_COUNT = 28
 
-    def scrape(self):
-        # needs to be implemented
-        pass
+class StLouisPersonScraper(Scraper):
+
+	def scrape(self):
+		yield self.scrape_people()
+
+	def scrape_people(self):
+		for ward_num in range(1, WARD_COUNT + 1):
+			person = self.scrape_alderman(ward_num)
+			yield person
+
+	def scrape_alderman(self, ward_num):
+		ward_url = "{}/ward-{}".format(ALDERMEN_HOME, ward_num)
+		alderman_url = self.alderman_url(ward_url)
+		alderman_page = self.lxmlize(alderman_url)
+
+		# person's name is the only <h1> tag on the page
+		name = alderman_page.xpath("//h1/text()")[0]
+
+		# initialize person object with appropriate data to tie back to
+		# a post in the jurisdiction's "Board of Aldermen" organization
+		district = "Ward {} Alderman".format(ward_num)
+		person = Person(name=name, district=district, role="Alderman", 
+										primary_org="legislature")
+
+		# set additional fields
+		person.image = alderman_page.xpath("//div/img/@src")[0]
+		phone_number = alderman_page.xpath("//strong[text()='Phone:']/../text()")[1].strip()
+		person.add_contact_detail(type="voice", value=phone_number)
+
+		# add sources
+		person.add_source(alderman_url, note="profile")
+		person.add_source(ward_url, note="ward")
+
+		return person
+
+	def alderman_url(self, ward_url):
+		ward_page = self.lxmlize(ward_url)
+		# each ward page contains a link to the current alderman's profile.
+		# the text of the link says "Email <Jane Doe>" where Jane Doe is the
+		# name of the alderman.
+		# find that link by selecting for <a> tags whose text contains 'Email'
+		return ward_page.xpath("//a[contains(text(), 'Email')]//@href")[0]
+
+	def lxmlize(self, url):
+		entry = requests.get(url)
+		page = html.fromstring(entry.text)
+		page.make_links_absolute(url)
+		return page
