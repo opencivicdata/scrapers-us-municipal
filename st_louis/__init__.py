900e65f0ea6bd34a6787b683827cd07f21393014
jesseilev <jesseilev@gmail.com>
2015-10-12 00:51:53 -0500

    comments; make WARD_COUNT a static property on StLouis class

--- st_louis/__init__.py
+++ st_louis/__init__.py
@@ -5,7 +5,14 @@ from .people import StLouisPersonScraper
 
 class StLouis(Jurisdiction):
     division_id = "ocd-division/country:us/state:mo/place:st_louis"
+
+    # FIXME
+    # Do we need jurisdiction_id? pupa did not auto-generate this field 
+    # like division_id, I made it myself.
+    # And is this the correct id? or should it be 
+    # .../place:st_louis/council ? or .../place:st_louis/government ?
     jurisdiction_id = "ocd-jurisdiction/country:us/state:mo/place:st_louis"
+    
     classification = "legislature"
     name = "St. Louis city Board of Aldermen"
     url = "https://www.stlouis-mo.gov/government/departments/aldermen/"
@@ -13,24 +20,14 @@ class StLouis(Jurisdiction):
         "people": StLouisPersonScraper,
     }
 
+    WARD_COUNT = 28
+
     def get_organizations(self):
-
-        WARD_COUNT = 28
-
-        #REQUIRED: define an organization using this format
-        #where org_name is something like Seattle City Council
-        #and classification is described here:
+        # create an Organization for the BofA
         org = Organization(name="St Louis Board of Aldermen", 
                            classification="legislature")
-
-        # OPTIONAL: add posts to your organizaion using thi format,
-        # where label is a human-readable description of the post (eg "Ward 8 councilmember")
-        # and role is the positoin type (eg conucilmember, alderman, mayor...)
-        # skip entirely if you're not writing a people scraper.
-        org.add_post(label="position_description", role="position_type")
-        for ward_num in range(1, WARD_COUNT + 1):
+        # add a post for each Ward
+        for ward_num in range(1, self.WARD_COUNT + 1):
             org.add_post(label="Ward {} Alderman".format(ward_num),
                          role="Alderman")
-
-        #REQUIIRED: yield the organization
         yield org
