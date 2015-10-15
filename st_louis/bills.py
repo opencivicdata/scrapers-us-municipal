6856485d7168ad992cc446f05f503dc84100cc56
jesseilev <jesseilev@gmail.com>
2015-10-15 00:47:06 -0500

    stub out skeleton of BaseBillScraper subclass

--- st_louis/bills.py
+++ st_louis/bills.py
@@ -1,4 +1,16 @@
 from pupa.scrape import Scraper
 from pupa.scrape import Bill
+from pupa.scrape import BaseBillScraper
 from .utils import Utils
 
+# we subclass pupa.scrape.BaseBillScraper -- special helper for bill scrapers
+# see https://github.com/opencivicdata/pupa/blob/master/ARCHITECTURE.md#pupascrape
+class StLouisBillScraper(BaseBillScraper):
+
+	def get_bill_ids(self):
+		# TODO
+		return None
+
+	def get_bill(self, bill_id):
+		# TODO
+		raise self.ContinueScraping()
