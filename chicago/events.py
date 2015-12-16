32c02cd25cc2beba496335712354e658dcb31d58
Forest Gregg <fgregg@uchicago.edu>
2015-12-16 09:31:36 -0600

    remove ems

--- chicago/events.py
+++ chicago/events.py
@@ -71,7 +71,7 @@ class ChicagoEventsScraper(LegistarEventsScraper) :
                     continue
                 else :
                     print(status_text)
-                    description = status_string[1]
+                    description = status_string[1].replace('--em--', '')
                     status = confirmedOrPassed(when)
             else :
                 status = confirmedOrPassed(when)
