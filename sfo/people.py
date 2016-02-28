6d809447b6b2fe5a4f9b670a4a05b92056b31f89
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-02-27 17:45:23 -0800

    Moved to using LegistarPersonScraper.

--- sfo/people.py
+++ sfo/people.py
@@ -1,95 +1,96 @@
 from pupa.scrape import Person, Organization
-from .legistar import LegistarScraper
-import logging
-from datetime import datetime
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-PEOPLE_PAGE = 'https://sfgov.legistar.com/People.aspx'
+from legistar.people import LegistarPersonScraper
+import datetime
 
 
-class SFPersonScraper(LegistarScraper):
-    base_url = 'https://sfgov.legistar.com/'
-
-    def People(self, follow_links=True) :
-        for page in self.pages(PEOPLE_PAGE) :
-            table = page.xpath(
-                "//table[@id='ctl00_ContentPlaceHolder1_gridPeople_ctl00']")[0]
-
-            for person, _, _ in self.parseDataTable(table):
-                if follow_links and type(person['Person Name']) == dict:
-                    if type(person['Web Site']) == dict:
-                        website_url = person['Web Site']['url']
-                        website = self.lxmlize(website_url)
-                        elem = website.xpath("//*[@class='sup_district']")
-                        if elem:
-                            person['District'] = elem[0].text.strip()
-
-                        contact_p = website.xpath("//*[@id='sup_right']/h2[contains(.,'Contact Info')]/following::p")
-                        if contact_p:
-                            # TODO: Split this into phone and address
-                            True
-
-                    detail_url = person['Person Name']['url']
-                    person_details = self.lxmlize(detail_url)
-                    img = person_details.xpath(
-                        "//img[@id='ctl00_ContentPlaceHolder1_imgPhoto']")
-                    if img :
-                        person['Photo'] = img[0].get('src')
-
-                    committee_table = person_details.xpath(
-                        "//table[@id='ctl00_ContentPlaceHolder1_gridDepartments_ctl00']")[0]
-                    committees = self.parseDataTable(committee_table)
-
-                    yield person, committees
-
-                else :
-                    yield person
-
+class SFPersonScraper(LegistarPersonScraper):
+    MEMBERLIST = 'https://sfgov.legistar.com/MainBody.aspx'
+    TIMEZONE = 'US/Pacific'
+    ALL_MEMBERS = "3:0"
 
     def scrape(self):
-        non_committees = {
-            'Board of Supervisors',
-            }
+        noncommittees = {'Board of Supervisors'}
+        people_d = {}
 
-        for person, committees in self.People() :
-            if person['District'] == None:
-                continue
+        for councilman, committees in self.councilMembers() :
 
-            district = person['District']
-            role = "Supervisor"
-            p = Person(
-                person['Person Name']['label'],
-                district=district,
-                primary_org="legislature",
-                role=role,
-                )
+            if 'url' in councilman['Web Site'] :
+                website_url = councilman['Web Site']['url']
+                website = self.lxmlize(website_url)
+                elem = website.xpath("//*[@class='sup_district']")
+                if elem:
+                    councilman['District'] = elem[0].text.strip()
 
-            if person['Photo'] :
-                p.image = person['Photo']
+            if 'url' in councilman['Person Name'] :
+                councilman_url = councilman['Person Name']['url']
 
-            if person["E-mail"]:
-                p.add_contact_detail(
-                    type="email",
-                    value=person['E-mail']['label'],
-                    note='E-mail'
-                    )
+                if councilman_url in people_d :
+                    people_d[councilman_url][0].append(councilman)
+                else :
+                    people_d[councilman_url] = [councilman], committees
 
-            if person['Web Site']:
-                p.add_link(person['Web Site']['url'])
-            p.add_source(person['Person Name']['url'], note='web')
+        for person_entries, committees in people_d.values() :
 
-            yield p
+            councilman = person_entries[-1]
+
+            p = Person(councilman['Person Name']['label'])
+
+            spans = [(self.toTime(entry['Start Date']).date(),
+                      self.toTime(entry['End Date']).date(),
+                      entry['District'])
+                     for entry in person_entries]
+
+            merged_spans = []
+            last_end_date = None
+            last_district = None
+            for start_date, end_date, district in sorted(spans) :
+                if last_end_date is None :
+                    span = [start_date, end_date, district]
+                elif (start_date - last_end_date) == datetime.timedelta(1) and district == last_district :
+                    span[1] = end_date
+                else :
+                    merged_spans.append(span)
+                    span = [start_date, end_date, district]
+
+                last_end_date = end_date
+                last_district = district
+
+            merged_spans.append(span)
+
+            for start_date, end_date, district in merged_spans :
+                district = councilman['District'].replace(' 0', ' ')
+                if end_date > datetime.date.today() :
+                    end_date = ''
+                else :
+                    end_date = end_date.isoformat()
+                print(start_date, end_date)
+                #p.add_term('Council Member', 'legislature',
+                #           district=district,
+                #           start_date=start_date.isoformat(),
+                #           end_date=end_date)
+
+            if councilman['Photo'] :
+                p.image = councilman['Photo']
+
+            if councilman["E-mail"]:
+                p.add_contact_detail(type="email",
+                                     value=councilman['E-mail']['url'],
+                                     note='E-mail')
+
+            if councilman['Web site']:
+                p.add_link(councilman['Web site']['url'], note='web site')
+
+            p.extras = {'Notes' : councilman['Notes']}
+            p.add_source(councilman['Person Name']['url'], note='web')
 
             for committee, _, _ in committees:
                 committee_name = committee['Department Name']
-                if committee_name and committee_name not in non_committees:
+                if committee_name not in noncommittees :
                     if committee['Title'] == 'Supervisor' :
                         committee['Title'] = 'Committee Member'
-                    p.add_membership(
-                        committee_name,
-                        role=committee["Title"],
-                        start_date=self.toTime(committee['Start Date']).date(),
-                        end_date=self.toTime(committee['End Date']).date(),
-                        )
+                    p.add_membership(committee_name,
+                                     role=committee["Title"],
+                                     start_date=self.mdY2Ymd(committee['Start Date']),
+                                     end_date=self.mdY2Ymd(committee['End Date'])
+                                     )
+            yield p
