78bac8447874f5d2df7b6b751b60e3a5444ba7d7
Forest Gregg <fgregg@uchicago.edu>
2016-10-10 16:24:18 -0500

    lametro start

---
+++ lametro/__init__.py
@@ -0,0 +1,34 @@
+# encoding=utf-8
+from pupa.scrape import Jurisdiction, Organization
+from .bills import LametroBillScraper
+from .people import LametroPersonScraper
+from .events import LametroEventScraper
+from .vote_events import LametroVoteEventScraper
+
+
+class Lametro(Jurisdiction):
+    division_id = "ocd-division/country:us/state:ca/county:los_angeles"
+    classification = "transit_authority"
+    name = "Los Angeles County"
+    url = "https://www.metro.net/"
+    scrapers = {
+        "bills": LametroBillScraper,
+        #"people": LametroPersonScraper,
+        #"events": LametroEventScraper,
+        #"vote_events": LametroVoteEventScraper,
+    }
+
+    def get_organizations(self):
+        #REQUIRED: define an organization using this format
+        #where org_name is something like Seattle City Council
+        #and classification is described here:
+        org = Organization(name="Los Angeles County Metropolitan Transportation Authority", classification="legislature")
+
+        # OPTIONAL: add posts to your organizaion using thi format,
+        # where label is a human-readable description of the post (eg "Ward 8 councilmember")
+        # and role is the positoin type (eg conucilmember, alderman, mayor...)
+        # skip entirely if you're not writing a people scraper.
+        #org.add_post(label="position_description", role="position_type")
+
+        #REQUIIRED: yield the organization
+        yield org
---
+++ lametro/bills.py
@@ -0,0 +1,207 @@
+from legistar.bills import LegistarBillScraper, LegistarAPIBillScraper
+from pupa.scrape import Bill, VoteEvent
+from pupa.utils import _make_pseudo_id
+import datetime
+import itertools
+import pytz
+import requests
+
+class LametroBillScraper(LegistarAPIBillScraper):
+    BASE_URL = 'http://webapi.legistar.com/v1/metro'
+    BASE_WEB_URL = 'https://metro.legistar.com'
+    TIMEZONE = "America/Los_Angeles"
+
+    VOTE_OPTIONS = {'aye' : 'yes',
+                    'nay' : 'no',
+                    'recused' : 'abstain',
+                    'present' : 'abstain'}
+
+    def session(self, action_date) :
+        localize = pytz.timezone(self.TIMEZONE).localize
+        # 2011 Kill Bill https://chicago.legistar.com/LegislationDetail.aspx?ID=907351&GUID=6118274B-A598-4584-AA5B-ABDFA5F79506
+        if action_date <  localize(datetime.datetime(2011, 5, 4)) :
+            return "2007"
+        # 2015 Kill Bill https://chicago.legistar.com/LegislationDetail.aspx?ID=2321351&GUID=FBA81B7C-8A33-4D6F-92A7-242B537069B3
+        elif action_date < localize(datetime.datetime(2015, 5, 6)) :
+            return "2011"
+        else :
+            return "2015"
+
+    def sponsorships(self, matter_id) :
+        for i, sponsor in enumerate(self.sponsors(matter_id)) :
+            sponsorship = {}
+            if i == 0 :
+                sponsorship['primary'] = True
+                sponsorship['classification'] = "Primary"
+            else :
+                sponsorship['primary'] = False
+                sponsorship['classification'] = "Regular"
+
+            sponsorship['name'] = sponsor['MatterSponsorName'].strip()
+            sponsorship['entity_type'] = 'organization'
+            
+            yield sponsorship
+
+    def actions(self, matter_id) :
+        old_action = None
+        for action in self.history(matter_id) :
+            action_date = action['MatterHistoryActionDate']
+            action_description = action['MatterHistoryActionName'].strip()
+            responsible_org = action['MatterHistoryActionBodyName']
+
+            if all((action_date, action_description, responsible_org)) :
+                action_date =  self.toTime(action_date).date()
+
+                bill_action = {'description' : action_description,
+                               'date' : action_date,
+                               'organization' : {'name' : responsible_org},
+                               'classification' : ACTION_CLASSIFICATION[action_description]}
+                if bill_action != old_action:
+                    old_action = bill_action
+                else:
+                    continue
+
+                if (action['MatterHistoryEventId'] is not None
+                    and action['MatterHistoryRollCallFlag'] is not None
+                    and action['MatterHistoryPassedFlag'] is not None) :
+                    
+                    # Do we want to capture vote events for voice votes?
+                    # Right now we are not? 
+                    bool_result = action['MatterHistoryPassedFlag']
+                    result = 'pass' if bool_result else 'fail'
+
+                    votes = (result, self.votes(action['MatterHistoryId'])) 
+                else :
+                    votes = (None, [])
+
+                yield bill_action, votes
+
+    def scrape(self) :
+        long_ago = datetime.datetime.now() - datetime.timedelta(3000)
+        for matter in self.matters(long_ago) :
+            matter_id = matter['MatterId']
+
+            date = matter['MatterIntroDate']
+            title = matter['MatterTitle']
+            identifier = matter['MatterFile']
+
+            if not all((date, title, identifier)) :
+                continue
+
+            bill_session = self.session(self.toTime(date))
+            bill_type = BILL_TYPES[matter['MatterTypeName']]
+
+            if identifier.startswith('S'):
+                alternate_identifiers = [identifier]
+                identifier = identifier[1:]
+            else:
+                alternate_identifiers = []
+
+            bill = Bill(identifier=identifier,
+                        legislative_session=bill_session,
+                        title=title,
+                        classification=bill_type,
+                        from_organization={"name":"Los Angeles County Metropolitan Transportation Authority"})
+
+            legistar_web = self.legislation_detail_url(matter_id)
+            legistar_api = self.BASE_URL + '/matters/{0}'.format(matter_id)
+
+            bill.add_source(legistar_web, note='web')
+            bill.add_source(legistar_api, note='api')
+
+            for identifier in alternate_identifiers:
+                bill.add_identifier(identifier)
+
+            for action, vote in self.actions(matter_id) :
+                act = bill.add_action(**action)
+
+                if action['description'] == 'Referred' :
+                    body_name = matter['MatterBodyName']
+                    act.add_related_entity(body_name,
+                                           'organization',
+                                           entity_id = _make_pseudo_id(name=body_name))
+
+                result, votes = vote
+                if result :
+                    vote_event = VoteEvent(legislative_session=bill.legislative_session, 
+                                           motion_text=action['description'],
+                                           organization=action['organization'],
+                                           classification=None,
+                                           start_date=action['date'],
+                                           result=result,
+                                           bill=bill)
+
+                    vote_event.add_source(legistar_web)
+                    vote_event.add_source(legistar_api + '/histories')
+
+                    for vote in votes :
+                        raw_option = vote['VoteValueName'].lower()
+                        clean_option = self.VOTE_OPTIONS.get(raw_option,
+                                                             raw_option)
+                        vote_event.vote(clean_option, 
+                                        vote['VotePersonName'].strip())
+
+                    yield vote_event
+
+
+            for sponsorship in self.sponsorships(matter_id) :
+                print(sponsorship)
+                bill.add_sponsorship(**sponsorship)
+
+            for topic in self.topics(matter_id) :
+                bill.add_subject(topic['MatterIndexName'].strip())
+
+            for attachment in self.attachments(matter_id) :
+                if attachment['MatterAttachmentName'] :
+                    bill.add_version_link(attachment['MatterAttachmentName'],
+                                          attachment['MatterAttachmentHyperlink'],
+                                          media_type="application/pdf")
+
+            bill.extras = {'local_classification' : matter['MatterTypeName']}
+
+            text = self.text(matter_id)
+
+            if text :
+                if text['MatterTextPlain'] :
+                    bill.extras['plain_text'] = text['MatterTextPlain']
+
+                if text['MatterTextRtf'] :
+                    bill.extras['rtf_text'] = text['MatterTextRtf'].replace(u'\u0000', '')
+
+            yield bill
+
+ACTION_CLASSIFICATION = {'WITHDRAWN' : None,
+                         'APPROVED' : None,
+                         'RECOMMENDED FOR APPROVAL' : None,
+                         'RECEIVED AND FILED' : None,
+                         'RECOMMENDED FOR APPROVAL AS AMENDED' : None,
+                         'APPROVED AS AMENDED' : None,
+                         'APPROVED THE CONSENT CALENDAR' : None,
+                         'DISCUSSED' : None,
+                         'ADOPTED' : None,
+                         'FORWARDED WITHOUT RECOMMENDATION' : None,
+                         'CARRIED OVER' : None,
+                         'RECEIVED' : None,
+                         'REFERRED' : None,
+                         'FORWARDED DUE TO ABSENCES AND CONFLICTS' : None}
+
+BILL_TYPES = {'Contract' : None,
+              'Budget' : None,
+              'Program' : None,
+              'Motion / Motion Response' : None,
+              'Policy' : None,
+              'Informational Report' : None,
+              'Fare / Tariff / Service Change' : None,
+              'Agreement' : None,
+              'Oral Report / Presentation' : None,
+              'Resolution' : None,
+              'Project' : None,
+              'Formula Allocation / Local Return' : None,
+              'Federal Legislation / State Legislation (Position)': None,
+              'Plan': None,
+              'Minutes': None,
+              'Ordinance': None,
+              'Appointment': None,
+              'Public Hearing': None,
+              'Application': None}
+
