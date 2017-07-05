0db7bf46810d73eda29ba94ff519be222dd12d76
reginafcompton <reginafcompton@gmail.com>
2017-07-05 10:25:39 -0500

    prepare files for codedeploy

---
+++ scripts/before_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Make directory for project. This may already exist but it's a good practice
+# to make sure this pipeline will work on a bare server.
+mkdir -p /home/datamade/scrapers-us-municipal-codedeploy
+
+# Decrypt files encrypted with blackbox
+cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/ && chown -R datamade.datamade . && sudo -H -u datamade blackbox_postdeploy
