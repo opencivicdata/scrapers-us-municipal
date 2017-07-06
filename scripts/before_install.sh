04aadb7c2315a92ea07c19ccfb9ed14426d0c2de
reginafcompton <reginafcompton@gmail.com>
2017-07-06 15:36:50 -0500

    Rename repo for codedeploy

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,7 +2,7 @@
 
 # Make directory for project. This may already exist but it's a good practice
 # to make sure this pipeline will work on a bare server.
-mkdir -p /home/datamade/scrapers-us-municipal-codedeploy
+mkdir -p /home/datamade/scrapers-us-municipal
 
 # Decrypt files encrypted with blackbox
 cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/ && chown -R datamade.datamade . && sudo -H -u datamade blackbox_postdeploy
