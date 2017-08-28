808d573866a2a38f6ca4599b4ba24992ce1d8baf
reginafcompton <reginafcompton@gmail.com>
2017-08-28 10:16:47 -0500

    Update ACTION_CLASSIFICATION dict

--- nyc/bills.py
+++ nyc/bills.py
@@ -172,6 +172,7 @@ BILL_TYPES = {'Introduction' : 'bill',
 
 
 ACTION_CLASSIFICATION = {
+    'Recalled by Council': None,
     'Tour Held by Committee' : None,
     'Hearing on P-C Item by Comm' : None,
     'Approved by Committee with Modifications and Referred to CPC' : 'committee-passage',
