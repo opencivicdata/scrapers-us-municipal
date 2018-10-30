6481020f397c463ea83370c7e6fb8de3b6ca9e23
reginafcompton <reginafcompton@gmail.com>
2018-10-30 13:37:03 -0500

    Add post in the init

--- lametro/__init__.py
+++ lametro/__init__.py
@@ -48,6 +48,10 @@ class Lametro(Jurisdiction):
                      'Nonvoting Board Member',  
                      division_id='ocd-division/country:us/state:ca')
 
+        org.add_post('District 7 Director, California Department of Transportation (Caltrans)',
+                     'Nonvoting Board Member',  
+                     division_id='ocd-division/country:us/state:ca')
+        
         org.add_post('Appointee of Los Angeles County City Selection Committee, North County/San Fernando Valley sector',
                      'Board Member',
                      division_id='ocd-division/country:us/state:ca/county:los_angeles/la_metro_sector:north_county_san_fernando_valley')
