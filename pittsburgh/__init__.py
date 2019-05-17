9da4928b7256ebde1b8664bc086e4738bb586e09
James O'Toole <jtotoole@gmail.com>
2019-05-17 19:10:54 -0400

    cleanup

--- pittsburgh/__init__.py
+++ pittsburgh/__init__.py
@@ -21,93 +21,14 @@ class Pittsburgh(Jurisdiction):
         "bills": PittsburghBillScraper,
     }
 
-    # legislative_sessions = []
+    legislative_sessions = []
 
-    # def get_legislative_sessions(self):
-    #     for year in range(2001, 2030):
-    #         legislative_sessions.append({"identifier": str(year),
-    #                 "name": str(year) + ' Session',
-    #                 "start_date": str(year) + '-01-01',
-    #                 "end_date": str(year) + '-12-31'})
-
-
-    legislative_sessions = [{"identifier": "2019",
-            "name": "2019 Session",
-            "start_date": "2019-01-01",
-            "end_date": "2019-12-31"},
-            {"identifier": "2018",
-            "name": "2018 Session",
-            "start_date": "2018-01-01",
-            "end_date": "2018-12-31"},
-            {"identifier": "2017",
-            "name": "2017 Session",
-            "start_date": "2017-01-01",
-            "end_date": "2017-12-31"},
-            {"identifier": "2016",
-            "name": "2016 Session",
-            "start_date": "2016-01-01",
-            "end_date": "2016-12-31"},
-            {"identifier": "2015",
-            "name": "2015 Session",
-            "start_date": "2015-01-01",
-            "end_date": "2015-12-31"},
-            {"identifier": "2014",
-            "name": "2015 Session",
-            "start_date": "2014-01-01",
-            "end_date": "2014-12-31"},
-            {"identifier": "2013",
-            "name": "2013 Session",
-            "start_date": "2013-01-01",
-            "end_date": "2013-12-31"},
-            {"identifier": "2012",
-            "name": "2012 Session",
-            "start_date": "2012-01-01",
-            "end_date": "2012-12-31"},
-            {"identifier": "2011",
-            "name": "2011 Session",
-            "start_date": "2011-01-01",
-            "end_date": "2011-12-31"},
-            {"identifier": "2015",
-            "name": "2010 Session",
-            "start_date": "2010-01-01",
-            "end_date": "2010-12-31"},
-            {"identifier": "2009",
-            "name": "2009 Session",
-            "start_date": "2009-01-01",
-            "end_date": "2009-12-31"},
-            {"identifier": "2008",
-            "name": "2008 Session",
-            "start_date": "2008-01-01",
-            "end_date": "2008-12-31"},
-            {"identifier": "2007",
-            "name": "2007 Session",
-            "start_date": "2007-01-01",
-            "end_date": "2007-12-31"},
-            {"identifier": "2008",
-            "name": "2006 Session",
-            "start_date": "2006-01-01",
-            "end_date": "2006-12-31"},
-            {"identifier": "2005",
-            "name": "2005 Session",
-            "start_date": "2005-01-01",
-            "end_date": "2005-12-31"},
-            {"identifier": "2004",
-            "name": "2004 Session",
-            "start_date": "2004-01-01",
-            "end_date": "2004-12-31"},
-            {"identifier": "2003",
-            "name": "2003 Session",
-            "start_date": "2003-01-01",
-            "end_date": "2003-12-31"},
-            {"identifier": "2008",
-            "name": "2002 Session",
-            "start_date": "2002-01-01",
-            "end_date": "2002-12-31"},
-            {"identifier": "2008",
-            "name": "2001 Session",
-            "start_date": "2001-01-01",
-            "end_date": "2001-12-31"},
-            ]
+    def get_legislative_sessions(self):
+        for year in range(2001, 2030):
+            legislative_sessions.append({"identifier": str(year),
+                                         "name": str(year) + ' Session',
+                                         "start_date": str(year) + '-01-01',
+                                         "end_date": str(year) + '-12-31'})
 
     def get_organizations(self):
         org = Organization(name="Pittsburgh City Council", classification="legislature")
@@ -116,26 +37,20 @@ class Pittsburgh(Jurisdiction):
                 "District {}".format(x),
                 "Councilmember",
                 division_id='ocd-division/country:us/state:pa/place:pittsburgh/council_district:{}'.format(x))
-
         yield org
 
         standing_committee = Organization(name='Standing Committee', classification='committee')
-
         standing_committee.add_source('http://pittsburghpa.gov/council/standing-committees', note='web')
-
         yield standing_committee
 
-
         mayor = Organization(name='Mayor', classification='executive')
-
+        mayor.add_post('Mayor', 'Mayor', division_id='ocd-division/country:us/state:pa/place:pittsburgh')
         mayor.add_source('http://pittsburghpa.gov/mayor/index.html', note='web')
-
         yield mayor
 
         city_clerk = Organization(name='City Clerk', classification='department')
         city_clerk.add_post('City Clerk', 'City Clerk', division_id='ocd-division/country:us/state:pa/place:pittsburgh')
         city_clerk.add_source('http://pittsburghpa.gov/clerk/', note='web')
-
         yield city_clerk
 
         pree = Person(name="Brenda Pree")
@@ -144,7 +59,6 @@ class Pittsburgh(Jurisdiction):
                       start_date=datetime.date(2017, 8, 29),
                       appointment=True)
         pree.add_source('http://pittsburghpa.gov/clerk/clerk-bio')
-
         yield pree
 
         doheny = Person(name="Mary Beth Doheny")
@@ -154,15 +68,14 @@ class Pittsburgh(Jurisdiction):
                         end_date=datetime.date(2017, 8, 28),
                         appointment=True)
         doheny.add_source('http://pittsburghpa.gov')
-
         yield doheny
+
+        # "All Members", frustratingly, has a Person entry in Pittsburgh
+        # Legistar, so the import trips without this. Going strong since 1816!
 
         all_members = Person(name="All Members")
         all_members.add_term('City Council',
                               'legislature',
                               start_date=datetime.date(1816, 3, 18))
         all_members.add_source('http://pittsburghpa.gov/council/index.html')
-
         yield all_members
-
-
--- pittsburgh/bills.py
+++ pittsburgh/bills.py
@@ -11,45 +11,44 @@ class PittsburghBillScraper(LegistarAPIBillScraper, Scraper):
     BASE_URL = 'http://webapi.legistar.com/v1/pittsburgh'
     BASE_WEB_URL = 'https://pittsburgh.legistar.com'
     TIMEZONE = "US/Eastern"
-
     VOTE_OPTIONS = {'aye' : 'yes',
                     'no' : 'no',
                     'abstain' : 'abstain',
                     'absent': 'absent',
                     'out of room': 'absent'}
 
-    def session(self, action_date) :
+    def session(self, action_date):
         return str(action_date.year)
 
-    def sponsorships(self, matter_id) :
-        for i, sponsor in enumerate(self.sponsors(matter_id)) :
+    def sponsorships(self, matter_id):
+        for i, sponsor in enumerate(self.sponsors(matter_id)):
             sponsorship = {}
-            if i == 0 :
+            if i == 0:
                 sponsorship['primary'] = True
                 sponsorship['classification'] = "Primary"
-            else :
+            else:
                 sponsorship['primary'] = False
                 sponsorship['classification'] = "Regular"
 
             sponsor_name = sponsor['MatterSponsorName'].strip()
 
-            if sponsor_name.startswith(('City Clerk',)) :
-                sponsorship['name'] = 'Office of the City Clerk'
+            if sponsor_name.startswith(('City Clerk',)):
+                sponsorship['name'] = 'City Clerk'
                 sponsorship['entity_type'] = 'organization'
-            else :
+            else:
                 sponsorship['name'] = sponsor_name
                 sponsorship['entity_type'] = 'person'
 
             if not sponsor_name.startswith(('Misc. Transmittal',
                                             'No Sponsor',
-                                            'Dept./Agency')) :
+                                            'Dept./Agency')):
                 yield sponsorship
 
-    def actions(self, matter_id) :
+    def actions(self, matter_id):
         old_action = None
         actions = self.history(matter_id)
 
-        for action in actions :
+        for action in actions:
             action_date = action['MatterHistoryActionDate']
             action_description = action['MatterHistoryActionName']
             responsible_org = action['MatterHistoryActionBodyName']
@@ -79,20 +78,20 @@ class PittsburghBillScraper(LegistarAPIBillScraper, Scraper):
 
             if (action['MatterHistoryEventId'] is not None
                 and action['MatterHistoryRollCallFlag'] is not None
-                and action['MatterHistoryPassedFlag'] is not None) :
+                and action['MatterHistoryPassedFlag'] is not None):
 
                 bool_result = action['MatterHistoryPassedFlag']
                 result = 'pass' if bool_result else 'fail'
 
                 votes = (result, self.votes(action['MatterHistoryId']))
-            else :
+            else:
                 votes = (None, [])
 
             yield bill_action, votes
 
-    def scrape(self, window=3) :
+    def scrape(self, window=3):
         n_days_ago = datetime.datetime.utcnow() - datetime.timedelta(float(window))
-        for matter in self.matters(n_days_ago) :
+        for matter in self.matters(n_days_ago):
             matter_id = matter['MatterId']
 
             date = matter['MatterIntroDate']
@@ -108,7 +107,7 @@ class PittsburghBillScraper(LegistarAPIBillScraper, Scraper):
             if identifier in problem_bills:
                 continue
 
-            if not all((date, title, identifier)) :
+            if not all((date, title, identifier)):
                 continue
 
             bill_session = self.session(self.toTime(date))
@@ -128,19 +127,17 @@ class PittsburghBillScraper(LegistarAPIBillScraper, Scraper):
                         legislative_session=bill_session,
                         title=title,
                         classification=ocd_bill_type,
-                        from_organization={"name":"Pittsburgh City Council"})
+                        from_organization={"name": "Pittsburgh City Council"})
 
             legistar_web = matter['legistar_url']
-
             legistar_api = 'http://webapi.legistar.com/v1/pittsburgh/matters/{0}'.format(matter_id)
-
             bill.add_source(legistar_web, note='web')
             bill.add_source(legistar_api, note='api')
 
             for identifier in alternate_identifiers:
                 bill.add_identifier(identifier)
 
-            for action, vote in self.actions(matter_id) :
+            for action, vote in self.actions(matter_id):
                 responsible_person = action.pop('responsible person')
                 act = bill.add_action(**action)
 
@@ -149,15 +146,16 @@ class PittsburghBillScraper(LegistarAPIBillScraper, Scraper):
                                             'person',
                                             entity_id = _make_pseudo_id(name=responsible_person))
 
-                if action['description'] == 'Referred' :
+                if action['description'] == 'Referred':
                     body_name = matter['MatterBodyName']
-                    if body_name != 'City Council' :
+                    if body_name != 'City Council':
                         act.add_related_entity(body_name,
                                                'organization',
                                                entity_id = _make_pseudo_id(name=body_name))
 
                 result, votes = vote
-                if result :
+
+                if result:
                     vote_event = VoteEvent(legislative_session=bill.legislative_session,
                                            motion_text=action['description'],
                                            organization=action['organization'],
@@ -169,7 +167,7 @@ class PittsburghBillScraper(LegistarAPIBillScraper, Scraper):
                     vote_event.add_source(legistar_web)
                     vote_event.add_source(legistar_api + '/histories')
 
-                    for vote in votes :
+                    for vote in votes:
                         raw_option = vote['VoteValueName'].lower()
                         clean_option = self.VOTE_OPTIONS.get(raw_option,
                                                              raw_option)
@@ -179,27 +177,26 @@ class PittsburghBillScraper(LegistarAPIBillScraper, Scraper):
                     yield vote_event
 
 
-            for sponsorship in self.sponsorships(matter_id) :
+            for sponsorship in self.sponsorships(matter_id):
                 bill.add_sponsorship(**sponsorship)
 
-            for topic in self.topics(matter_id) :
+            for topic in self.topics(matter_id):
                 bill.add_subject(topic['MatterIndexName'].strip())
 
-            for attachment in self.attachments(matter_id) :
-                if attachment['MatterAttachmentName'] :
+            for attachment in self.attachments(matter_id):
+                if attachment['MatterAttachmentName']:
                     bill.add_version_link(attachment['MatterAttachmentName'],
                                           attachment['MatterAttachmentHyperlink'],
                                           media_type="application/pdf")
 
             bill.extras = {'local_classification' : matter['MatterTypeName']}
-
             text = self.text(matter_id)
 
-            if text :
-                if text['MatterTextPlain'] :
+            if text:
+                if text['MatterTextPlain']:
                     bill.extras['plain_text'] = text['MatterTextPlain']
 
-                if text['MatterTextRtf'] :
+                if text['MatterTextRtf']:
                     bill.extras['rtf_text'] = text['MatterTextRtf'].replace(u'\u0000', '')
 
             yield bill
@@ -269,8 +266,6 @@ ACTION = {'Committee Report':
           'Signed by the Mayor':
               {'ocd': 'executive-signature', 'order': 4}
         }
-
-
 
 BILL_TYPES = {'Ordinance' : 'ordinance',
               'Resolution' : 'resolution',
